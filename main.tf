fa1aca29d6d7fee15254d158d215f1a6ded899e1
marines2002 <nigel.charlton@gmail.com>
2020-06-06 12:39:44 +0100

    add web app

--- main.tf
+++ main.tf
@@ -1,11 +1,27 @@
-# Configure the Microsoft Azure Provider.
+
 provider "azurerm" {
   version = "~>1.31"
 }
 
-# Create a resource group
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.prefix}-hca-kestral-rg"
+resource "azurerm_resource_group" "kestrel" {
+  name     = "${var.prefix}-hca-kestrel-rg"
   location = var.location
   tags     = var.tags
+}
+
+resource "azurerm_app_service_plan" "kestrel" {
+    name                = "${var.prefix}-hca-kestrel-appserviceplan"
+    location            = azurerm_resource_group.kestrel.location
+    resource_group_name = azurerm_resource_group.kestrel.name
+    sku {
+        tier = "Standard"
+        size = "S1"
+    }
+}
+
+resource "azurerm_app_service" "kestrel" {
+    name                = "kestrelAppService"
+    location            = azurerm_resource_group.kestrel.location
+    resource_group_name = azurerm_resource_group.kestrel.name
+    app_service_plan_id = azurerm_app_service_plan.kestrel.id
 }
