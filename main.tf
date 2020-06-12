8baf632b8d8eb19ae88938f79c82ef120ca5e072
marines2002 <nigel.charlton@gmail.com>
2020-06-12 08:59:48 +0100

    Update main.tf
    added ui appservice

--- main.tf
+++ main.tf
@@ -29,6 +29,17 @@ resource "azurerm_app_service" "kestrel" {
     }
 }
 
+resource "azurerm_app_service" "kestrel-ui" {
+    name                = "${var.environment}-hca-kestrel-appservice-ui"
+    location            = azurerm_resource_group.kestrel.location
+    resource_group_name = azurerm_resource_group.kestrel.name
+    app_service_plan_id = azurerm_app_service_plan.kestrel.id
+
+    site_config {
+         dotnet_framework_version = "v4.0"    
+    }
+}
+
 resource "azurerm_sql_server" "kestrel" {
   name                         = "${var.environment}-hca-kestrel-sqlserver"
   resource_group_name          = azurerm_resource_group.kestrel.name
